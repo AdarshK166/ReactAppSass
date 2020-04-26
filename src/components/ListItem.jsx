b6cc2a930a5fc159a54c371efd0d9e85ab412708
AdarshK166 <adarshkolvekar@gmail.com>
2020-04-26 20:10:22 +0530

    Update ListItem.jsx

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-//import "../style/video.css";
 
 const ListItem = ({ video, handleVideoSelect }) => {
   return (
     <div onClick={() => handleVideoSelect(video)} className="video-item item">
-      <img
-        className="itemThumb"
-        src={video.snippet.thumbnails.medium.url}
-        //alt={video.snippet.description}
-      />
+      <img className="itemThumb" src={video.snippet.thumbnails.medium.url} />
       <div className="titleDisplay">
         <div className="itemTitle">{video.snippet.title}</div>
       </div>
